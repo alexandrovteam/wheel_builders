4f59234d25bf75b6a10521addfaf96cc44e08dc3
Artem Tarasov <artem.tarasov@embl.de>
2016-10-26 17:21:32 +0200

    fix linux builder

--- build_linux_wheels.sh
+++ build_linux_wheels.sh
@@ -9,9 +9,8 @@ build_cmd="
 cd $mnt/$SUBMODULE/build &&\
 cmake -DCMAKE_BUILD_TYPE=Release .. &&\
 make -B -j8 $TARGET && cd $mnt && rm -r dist/*;
-source activate py2; python setup.py bdist_wheel;
-source activate py3; python setup.py bdist_wheel;
-source activate py3; bash -c 'for fn in `ls dist/*.whl`; do auditwheel repair \$fn; done'
+source activate py2; pip install -U wheel; python setup.py bdist_wheel --universal --plat-name manylinux1_x86_64;
+source activate py3; pip install -U wheel; python setup.py bdist_wheel --universal --plat-name manylinux1_x86_64;
 "
 
 image=devtoolset-3
